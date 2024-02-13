1d7528b6401c58256447e6321acbe66dcd0a620e
hatimhussain52 <157620936+hatimhussain52@users.noreply.github.com>
2024-02-12 23:34:46 -0500

    created a conflict

--- src/sologithub/HelloWorld.java
+++ src/sologithub/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("hello world");
-		System.out.println("alternate");
+		System.out.println("created conflict");
 		
 	}
 
